4c4deefceb28589a24d1c89185acdda8bdd63383
Weyler Antonio Uicab Pat <intel1981@gmail.com>
2019-03-24 13:18:01 -0500

    Update migration escuelas
    Se agregan nuevos campos a la entidad ESCUELA

--- database/migrations/2019_03_07_234059_create_escuelas_table.php
+++ database/migrations/2019_03_07_234059_create_escuelas_table.php
@@ -14,13 +14,26 @@ class CreateEscuelasTable extends Migration
     public function up()
     {
         Schema::create('escuelas', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('cct',10)->unique();
-            $table->string('nombre',120);
-            $table->string('nivel', 60);
-            $table->string('turno', 60);
-            $table->string('sostenimiento', 60);
-            $table->boolean('status')->default(false);
+            $table->bigIncrements('id'); //1
+            $table->smallInteger('tipo_id'); //2
+            $table->smallInteger('nivel_id'); //3
+            $table->smallInteger('servicio_id'); //4
+            $table->string('nombre',120); //5
+            $table->string('cct',10)->unique();  //6
+            $table->string('incorporacion',20)->nullable(); //7
+            $table->string('turno', 60); //8
+            $table->string('sostenimiento', 60); //9
+            $table->string('direccion',120); //10
+            $table->string('exterior',60); //11
+            $table->string('interior',60)->nullable(); //12
+            $table->string('referencia',120); //13
+            $table->string('colonia', 120); //14
+            $table->string('codpost',5); //15
+            $table->string('pais',30); //16
+            $table->string('estado',30); //17-entidad
+            $table->string('delegacion',60); //18-municipio
+            $table->string('localidad',60); //19
+            $table->boolean('status')->default(false); //20
             $table->softDeletes();
             $table->timestamps();
         });
